221c908583152f2bbcd7985213fd3ee2682f87bc
Cory Pruce <cwpruce@gmail.com>
2013-07-24 13:06:22 -0400

    first commit

---
+++ GFChar.cpp
@@ -0,0 +1,9 @@
+//
+//  GFChar.cpp
+//  GFChar
+//
+//  Created by Cory Pruce on 7/10/13.
+//  Copyright (c) 2013 Pitzer College. All rights reserved.
+//
+
+#include <iostream>
---
+++ MKPS.cpp
@@ -0,0 +1,9 @@
+//
+//  MKPS.cpp
+//  GFChar
+//
+//  Created by Cory Pruce on 7/12/13.
+//  Copyright (c) 2013 Pitzer College. All rights reserved.
+//
+
+#include <iostream>
