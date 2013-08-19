c5f19c2b1685d5ada285ad449cb728a7dfd797fe
Cory Pruce <cwpruce@gmail.com>
2013-08-19 12:19:44 -0400

    updated power

--- GFChar.cpp
+++ GFChar.cpp
@@ -36,7 +36,6 @@ void FillLogArrays () {
 } 
 
 unsigned char Product (unsigned char A, unsigned char B) { 
-    cout << "" << endl;
     if ((A == 0) || (B == 0)) return (0); 
     else return (ALog[(Log[A] + Log[B]) % (GF-1)]); 
 }
@@ -53,17 +52,13 @@ unsigned char inverse(unsigned char y)
     return Quotient(1, y);
 }
 unsigned char power (unsigned char a, unsigned char b){
-    int pow;
-    for(unsigned char i = 0; i < b; i++){
-        //cout << static_cast<int>(Log[a]) << endl;
-        pow+=Log[a];
-    }
-    return ALog[pow % (GF-1)];
+    if ((a == 1) || (b == 0)) return (1);
+    return ALog[Product(b, Log[a])];
 }
 /*int main(){
     FillLogArrays();
     //cout << static_cast<int>(Product(Product(8, 8), 8)) << endl;
-    cout << static_cast<int>(Product(32, 32)) << endl;
-    cout << static_cast<int>(power(32, 2)) << endl;
+    cout << static_cast<int>(Product(Product(32, 32), 32)) << endl;
+    cout << static_cast<int>(power(32, 3)) << endl;
     
 }*/
